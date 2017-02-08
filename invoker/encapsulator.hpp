6e8731b60867793aae2683f44f4acfeabc6c63cd
/a <maniotis@merlin>
2017-02-08 03:03:55 +0100

    .

---
+++ invoker/encapsulator.hpp
@@ -0,0 +1,135 @@
+#ifndef ATC_ENCAPSULATOR_HPP_INC
+#define ATC_ENCAPSULATOR_HPP_INC
+
+#include <type_traits>
+#include <utility>
+
+namespace metafun     {
+namespace invoker_dtl {
+    
+    template<typename> struct capsule;
+
+    template<typename T>
+    struct capsule<T const&&>
+    {
+	explicit capsule(T       && ref) : ref_(std::move(ref)) { }	
+	explicit capsule(T const && ref) : ref_(std::move(ref)) { }
+
+	T const && leave() const { return std::move(ref_); }
+	T const && ref_;
+
+
+	template<typename S
+		 , typename = typename std::enable_if<std::is_convertible<T&, S&>::value >::type
+		 >
+	operator S const&()  { return const_cast<S&>(static_cast<S const&>(leave())); }
+    };
+    
+    template<typename T>
+    struct capsule<T&&> : public capsule<T const&&>
+    {
+	explicit capsule(T&& ref) : capsule<T const&&>(static_cast<T const&&>(ref)) { }
+	T&& leave() const
+	{
+	    return const_cast<T&&>( capsule<T const&&>::leave() );
+	}
+
+	template<typename S
+		 , typename = typename std::enable_if<std::is_convertible<T&, S&>::value >::type
+		 >
+	operator S&()  { return const_cast<S&>(static_cast<S const&>(leave())); }
+
+    };
+
+    template<typename T>
+    using rval_referenced = typename std::remove_reference<T>::type &&;
+    
+    template<typename... RvalRefArgs>
+    struct encapsulator : public capsule<RvalRefArgs >...
+    {
+	explicit encapsulator(RvalRefArgs... args) : capsule<RvalRefArgs>(std::move(args))... { }
+
+
+	template<typename... PermutedRvalRefArgs>
+	    explicit encapsulator(encapsulator<PermutedRvalRefArgs...>&& other) : capsule<RvalRefArgs>(static_cast<rval_referenced<RvalRefArgs> >(static_cast<RvalRefArgs&>(other)))... {}
+	
+	template<typename RvalRef>
+	RvalRef access() 
+	{
+	   return  static_cast<capsule<RvalRef>  &>(*this).leave(); 
+	}
+	
+	template<typename... permuted, typename F>
+	auto permute_and_invoke(F&& f)
+	{
+	    // two cases to distinguish in compile time via SFINAE in the
+            // private part ot this class:
+	    // identical permutation and true permutation.
+	    return permute_and_invoke_<permuted...>(std::forward<F>(f), nullptr); 
+	}
+	
+    private:    
+
+	// The second argument of the two functions permuted_and_invoke_ below ( ellipsis  verus std::nullptr_t)
+	// merely exists to guide the template call resolution in permuted_and_invoke above.
+	
+	// realise a permutation != identity	
+	template<typename... permuted, typename F>
+	auto  permute_and_invoke_(F&& f, ...)
+	{
+	    return encapsulator<rval_referenced<permuted>...>( this->access<permuted>()...).template permute_and_invoke<permuted...>(std::forward<F>(f));
+	}
+
+	/*
+	// realise the identity permutation (no permuting at all)
+	template<typename... permuted
+		 , typename F
+		 , typename = typename std::enable_if<first_arg_list_fits_second_arg_list<arg_list<permuted...>, arg_list<RvalArgs...> >::value>::type
+		 >
+        auto permute_and_invoke_(F&& f, std::nullptr_t)
+	{
+	    return std::forward<F>(f)(access<RvalArgs>()...);
+	}
+	*/
+
+    public:
+	template<typename F>
+	    auto invoke_(F&& f)
+	{
+	    return std::forward<F>(f)(access<RvalRefArgs>()...);
+	}
+    };
+    
+    template<typename X>
+    struct constructor
+    {
+	template<typename... Args>
+	X operator()(Args&&... args) const { return X(std::forward<Args>(args)...); } 
+    };
+
+    template<typename... Args>
+    struct invoker
+    {
+	template<typename F, typename... PermutedArgs>
+	auto operator()(F&& f, PermutedArgs&&... permuted_args ) const
+	{
+	    return encapsulator<rval_referenced<Args>...>(
+		encapsulator<rval_referenced<PermutedArgs>...>(
+		    static_cast<rval_referenced<PermutedArgs> >(permuted_args)... ) ).invoke_(std::forward<F>(f));
+
+	}
+    };
+    
+} // namesapce invoker_dtl
+
+    template<typename F, typename... Args>
+    auto invoke(F&& f, Args&&... args)
+    {
+	using invoker = invoker_dtl::invoker<invoker_dtl::rval_referenced<Args>...>;
+
+	return invoker()(std::forward<F>(f), std::forward<Args>(args)...);
+    }
+
+} // namespace metafun
+    
+#endif // ATC_ENCAPSULATOR_HPP_INC
---
+++ invoker/signature.hpp
@@ -0,0 +1,145 @@
+#ifndef METAFUN_SIGNATURE_COMPARISON_INC
+#define METAFUN_SIGNATURE_COMPARISON_INC
+
+#include "../list.hpp"
+#include "../set.hpp"
+
+namespace metafun       {
+namespace invoker_dtl   {
+namespace signature_dtl { 
+
+    template<typename T1, typename T2>
+    struct first_converts_to_second
+    {
+	enum
+	{
+	    value = std::is_convertible<T1, T2>::value
+/*
+	    value =
+	    std::is_convertible<typename std::decay<T1>::type&,
+	                        typename std::decay<T2>::type&>::value
+
+	    &&
+	     ( ! std::is_const<typename std::remove_reference<T1>::type>::value
+	       ||
+	       std::is_const<typename std::remove_reference<T2>::type>::value )
+*/
+	};
+
+    };
+
+    template<typename X, typename List2>
+    struct count_fits;
+
+    template<typename X>
+    struct count_fits<X, metafun::list<> >
+    {
+	enum { value = 0 };
+    };
+
+    template<typename X, typename H, typename... T>
+    struct count_fits<X, metafun::list<H, T...> >
+    {
+	enum { value = first_converts_to_second<X, H>::value
+	       +
+	       count_fits<X, metafun::list<T...> >::value };
+    };
+
+    template<typename Number, typename Count>
+    using accumulate =
+	typename Number::template rebind< eval<Number>() + eval<Count>() >;
+
+    template<typename List1, typename List2>
+    struct first_signature_converts_to_second;
+
+    template<>
+    struct first_signature_converts_to_second<metafun::list<>, metafun::list<> >
+    {
+	static constexpr bool eval() { return true; }
+    };
+
+    template<typename T>
+    using no_more_often_than_once =
+	metafun::bool_< (eval<T>() <= 1) >;
+    
+    template<typename... Args1, typename... Args2>
+    struct first_signature_converts_to_second< metafun::list<Args1...>,
+					       metafun::list<Args2...> >
+    {
+	static_assert(sizeof...(Args1) == sizeof...(Args2), "");
+	
+	static constexpr bool eval()
+	{
+	    using intermediate_result =
+		list
+		<
+		    count_fits<Args1, metafun::list<Args2...> >...
+	        >;
+	    
+	   using total_count = 
+		typename intermediate_result::template apply
+		<
+		    metafun::fold<accumulate, type<int>::template instance<0> >::
+		    template right
+	       >;
+
+	   using no_more_than_once_test =
+	       typename intermediate_result::
+	       
+	       template apply_pointwise
+	       <
+		   no_more_often_than_once
+	       > ::
+	       
+	       template apply<metafun::logic::all>;
+	   
+	   return
+	       (metafun::eval<total_count>() == sizeof...(Args2))
+	       &&
+	       metafun::eval<no_more_than_once_test>();
+	}
+    };
+    	
+} // namespace metafun       
+} // namespace invoker_dtl   
+} // namespace signature_dtl  
+
+#endif //  METAFUN_SIGNATURE_COMPARISON_INC
+
+
+/*
+template<typename T1, typename T2>
+    struct first_fits_second
+    {
+    };
+
+    template<typename...> struct arg_list;
+
+    template<template<typename, typename> class Pred, typename, typename>
+    struct for_all;
+
+    template<template<typename, typename> class Pred, typename... Args>
+    struct for_all<Pred, arg_list<>, arg_list<Args...> >
+    {
+	enum { value = true };
+    };
+
+    template<template<typename, typename> class Pred,
+	     typename Arg0, typename Arg1, typename... Other0, typename... Other1>
+    struct for_all<Pred, arg_list<Arg0, Other0...>, arg_list<Arg1, Other1...> >
+    {
+	enum { value = Pred<Arg0, Arg1>::value && for_all<Pred, arg_list<Other0...>, arg_list<Other1...> >::value };
+    };
+
+    template<typename, typename>
+    struct first_arg_list_fits_second_arg_list;
+
+    template<typename... Args1, typename... Args2>
+    struct first_arg_list_fits_second_arg_list<arg_list<Args1...>, arg_list<Args2...> >
+    {
+	enum { value =
+	       (sizeof...(Args1) == sizeof...(Args2))
+	       &&
+	       for_all<first_fits_second, arg_list<Args1...>, arg_list<Args2...> >::value };
+    };
+*/
