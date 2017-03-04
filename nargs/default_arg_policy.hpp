0624264676b6ded2348710aac53102121dd5bdaa
/a <maniotis@merlin>
2017-03-04 02:57:50 +0100

    guter Zwischenstand beim Einbau von bind/default_args

---
+++ nargs/default_arg_policy.hpp
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 Andreas Milton Maniotis
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef NARGS_DEFAULT_ARG_POLICY_INC
+#define NARGS_DEFAULT_ARG_POLICY_INC
+
+namespace nargs
+{
+    enum class default_args : bool { yes = true, no = false };
+} 
+    
+#endif // NARGS_DEFAULT_ARG_POLICY_INC
